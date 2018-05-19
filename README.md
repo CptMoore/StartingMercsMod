6bb273c118e5992420ec6ca9e03c246f92259762
CptMoore <cptmoore@pluto>
2018-05-19 13:24:16 +0200

    added option to load new pilots into a save game

--- README.md
+++ README.md
@@ -20,13 +20,15 @@ or
 Setting | Type | Default | Description
 --- | --- | --- | ---
 addRandomMercsCount | int | 3 | amount of random mercs to add to roster
-randomMercQuality | int | 1 | merc quality is based on difficulty, choose a value between 1 and 5.
+randomMercQuality | int | 1 | merc quality is based on difficulty, choose a value between 1 and 5, corresponds to a planets difficulty level
 roninChance | float | 0.08f | chance that a random ronin is part of the starting roster
 addRoninMercs | string[] | [] | a list of ronin pilot to always add, all backers and ronins under StreamingAssets\data\pilot can be chosen. e.g. `["pilot_ronin_Kraken", "pilot_backer_Chang"]`
+addRoninsOnSavegameLoad | bool | false | if set to true, the ronins mentioned under addRoninMercs are added to the barracks when loading a save game, great for adding pilots to an existing save game
+allowMultipleRoninCopies | bool | false | allow multiple copies of the same ronin when adding them through this mod, useful to revive dead ronins or get back fired ronins
 
 Note:
 
-The lists `StartingMechWarriors` and `StartingMechWarriorPortraits` in `SimGameConstants.json` need to be empty in order for this to work. ModTek in-memory patching should do that for you automatically. The game uses the first mercs in the roster to add as pilots for the starting mission. In any case mercs added through this mod will appear in the roster after the mission.
+The lists `StartingMechWarriors` and `StartingMechWarriorPortraits` in `SimGameConstants.json` needs to be empty in order for this to work. ModTek in-memory patching should do that for you automatically. The game uses the first mercs in the roster to add as pilots for the starting mission. In any case mercs added through this mod will appear in the roster after the mission.
 
 ## Download
 
--- source/Control.cs
+++ source/Control.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using HBS.Logging;
@@ -15,6 +16,8 @@ namespace StartingMercsMod
         public int randomMercQuality = 1; //1-5
         public float roninChance = 0.08f;
         public string[] addRoninMercs = { };
+        public bool addRoninsOnSavegameLoad = false;
+        public bool allowMultipleRoninCopies = false;
     }
 
     public static class Control
@@ -38,32 +41,77 @@ namespace StartingMercsMod
             mod.Logger.Log("Loaded " + mod.Name);
         }
 
+        // starting a new game
         [HarmonyPatch(typeof(SimGameState), "FirstTimeInitializeDataFromDefs")]
         public static class SimGameStateFirstTimeInitializeDataFromDefsPatch
         {
             public static void Postfix(SimGameState __instance)
             {
-                settings.addRoninMercs.Do(roninId =>
-                {
-                    __instance.AddPilotToRoster(roninId);
-                });
-
-                if (settings.addRandomMercsCount > 0)
-                {
-                    List<PilotDef> roninPilotDefs;
-                    var pilotDefs = __instance.PilotGenerator.GeneratePilots(
-                        settings.addRandomMercsCount*2,
-                        settings.randomMercQuality,
-                        settings.roninChance,
-                        out roninPilotDefs);
-
-                   var all = pilotDefs.Union(roninPilotDefs).ToList();
-                   all.Shuffle();
-                   all.Take(settings.addRandomMercsCount)
-                       .Do(pd => mod.Logger.Log("Adding pilot " + pd.Description.Id + " to roster"))
-                       .Do(pd => __instance.AddPilotToRoster(pd));
-                }
+                __instance.AddRoninsToRoster();
+                __instance.AddRandomMercsToRoster();
+                __instance.DumpRoster();
             }
+        }
+
+        // loading up a save game
+        [HarmonyPatch(typeof(SimGameState), "Rehydrate")]
+        public static class SimGameStateRehydratePatch
+        {
+            public static void Postfix(SimGameState __instance)
+            {
+                if (!settings.addRoninsOnSavegameLoad)
+                {
+                    return;
+                }
+
+                __instance.AddRoninsToRoster();
+                __instance.DumpRoster();
+            }
+        }
+
+        public static void AddRoninsToRoster(this SimGameState sim)
+        {
+            settings.addRoninMercs.Do(roninId =>
+            {
+                if (settings.allowMultipleRoninCopies)
+                {
+                    sim.UsedRoninIDs.Remove(roninId);
+                }
+                else if (sim.UsedRoninIDs.Contains(roninId))
+                {
+                    mod.Logger.Log("Not adding pilot " + roninId + " to roster, as pilot was already hired");
+                    return;
+                }
+
+                mod.Logger.Log("Adding pilot " + roninId + " to roster");
+                sim.AddPilotToRoster(roninId);
+            });
+        }
+
+        public static void AddRandomMercsToRoster(this SimGameState sim)
+        {
+            if (settings.addRandomMercsCount <= 0)
+            {
+                return;
+            }
+
+            List<PilotDef> roninPilotDefs;
+            var pilotDefs = sim.PilotGenerator.GeneratePilots(
+                settings.addRandomMercsCount * 2,
+                settings.randomMercQuality,
+                settings.roninChance,
+                out roninPilotDefs);
+
+            var all = pilotDefs.Union(roninPilotDefs).ToList();
+            all.Shuffle();
+            all.Take(settings.addRandomMercsCount)
+                .Do(pd => mod.Logger.Log("Adding pilot " + pd.Description.Id + " to roster"))
+                .Do(pd => sim.AddPilotToRoster(pd));
+        }
+
+        public static void DumpRoster(this SimGameState sim)
+        {
+            sim.PilotRoster.Do(pilot => mod.Logger.LogDebug("Roster contains pilot " + pilot.Name));
         }
     }
 }
